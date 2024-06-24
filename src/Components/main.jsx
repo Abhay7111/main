6ba1e807007808db9e7adde3fb56b52fd414dede
Abhay vishwakarma <142080096+Abhay7111@users.noreply.github.com>
2024-06-25 04:23:28 +0530

    d 5

--- src/Components/main.jsx
+++ src/Components/main.jsx
@@ -4,15 +4,28 @@ function Main() {
   return (
     <div className='w-full h-full bg-zinc-900 flex items-center justify-center'>
      {/* This is left Nav */}
-          <div className='w-20 sm:w-32 h-full bg-zinc-900 border-r border-clr1 flex flex-col items-center justify-between'></div>
+          <div className='w-20 h-full py-2 bg-zinc-900 border-r border-clr1 flex flex-col items-center justify-between'>
+               <div className='w-fit max-h-[90vh] bg-green-400/0 flex flex-col items-center justify-start gap-2'>
+                    <div className='size-10 border border-clr2 mb-10 rounded-lg flex items-center justify-center'>MN</div>
+                    <div className='size-10 rounded-lg flex items-center justify-center text-xl hover:bg-zinc-50/5 cursor-pointer transition-all'><i class="ri-home-4-line"></i></div>
+                    <div className='size-10 rounded-lg flex items-center justify-center text-xl hover:bg-zinc-50/5 cursor-pointer transition-all'><i class="ri-pages-line"></i></div>
+                    <div className='size-10 rounded-lg flex items-center justify-center text-xl hover:bg-zinc-50/5 cursor-pointer transition-all'><i class="ri-money-rupee-circle-line"></i></div>
+                    <div className='size-10 rounded-lg flex items-center justify-center text-xl hover:bg-zinc-50/5 cursor-pointer transition-all'><i class="ri-contacts-line"></i></div>
+               </div>
+               <div className='w-fit max-h-[10vh]'>
+                    <div className='size-10 border border-clr2 rounded-full overflow-hidden'></div>
+               </div>
+          </div>
           {/* This is right display */}
           <div className='w-full h-full bg-zinc-900'>
                {/* This is Nav */}
                <div className='w-full h-20 border-b border-clr1'></div>
                {/* This is main display */}
-               <div className='w-full bg-red-400/20 overflow-hidden overflow-y-auto calc-h-20'>
-                    <div className='w-full h-full'></div>
-                    <div className='w-full h-full bg-zinc-900 mt-5'></div>
+               <div className='w-full overflow-hidden overflow-y-auto calc-h-20'>
+                    <div className='w-full h-auto'>
+                    <div className='w-full h-full'>d</div>
+                    <div className='w-full h-[100vh] bg-zinc-900 mt-5'>5</div>
+                    </div>
                </div>
           </div>
     </div>
