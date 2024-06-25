55518d3bda9c79b8160943cda8f362adc7894e32
Abhay vishwakarma <142080096+Abhay7111@users.noreply.github.com>
2024-06-26 02:00:48 +0530

    Image updated

--- src/Components/Cards/Sitecard.jsx
+++ src/Components/Cards/Sitecard.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 
 function Sitecard({sites}) {
   return (
-    <div className='w-full sm:w-[290px] bg-green-400 rounded-lg p-1'>
-        <div className='w-full h-auto bg-yellow-500 rounded-lg'>
-          <img src={sites.image} className='w-full rounded-lg'/>
+    <a href={sites.url} target='_blank' className=' rounded-lg hover:scale-[1.02] transition-all hover:shadow-lg hover:shadow-zinc-800'>
+      <div className='w-full sm:w-[260px] bg-zinc-800 hover:bg-zinc-900 rounded-lg gap-3 flex flex-col items-start justify-start px-2 pt-2 pb-4 '>
+        <div className='w-full h-auto rounded-lg'>
+          <img src={sites.image} className='w-full sm:h-[200px] sm:object-cover rounded-lg border border-clr2'/>
+        </div>
+        <div className='w-full pl-3'>
+          <h1 className='text-xl font-semibold'>{sites.name}</h1>
+          <p className='text-sm mt-2 text-zinc-400'>Owned by <a href={sites.ownedurl} className='text-sm font-medium underline underline-offset-2 text-zinc-300'>{sites.owned}</a></p>
+          <p className='text-sm text-zinc-400'>Deploys from <a href={sites.platformurl} className='text-sm font-medium underline underline-offset-2 text-zinc-300'>{sites.platform}</a></p>
         </div>
     </div>
+    </a>
   )
 }
 
--- src/Components/Routs/Site.jsx
+++ src/Components/Routs/Site.jsx
@@ -4,32 +4,85 @@ import Sitecard from '../Cards/Sitecard'
 function Site() {
      const sites = [
           {
-               name:'gaama.top',
-               Owned:'gaama.top',
+               name:'Gaama.top',
+               owned:'gaama.top',
+               ownedurl:'',
                image:'https://img.freepik.com/free-vector/landing-page-template-with-web-design-concept_23-2147867627.jpg?t=st=1719331638~exp=1719335238~hmac=6f264662dbe98f9a29d3c3c2ce0a44542847e1cba8d15854b0a2194210e6ca85&w=740',
-               url:'',
+               url:'https://www.gaama.top/',
                platform:'Netlify',
+               platformurl:''
           },
           {
-               name:'gaama.top',
-               Owned:'gaama.top',
-               image:'https://img.freepik.com/free-photo/view-3d-computer-device-with-peripheral-devices_23-2150714047.jpg?t=st=1719332936~exp=1719336536~hmac=a78c13c3270a244eea55b0f53267a4bb52b0a52febd05679a4cb09fd5450019b&w=740',
-               url:'',
+               name:'Gaama.top',
+               owned:'gaama.top',
+               ownedurl:'',
+               image:'https://cdn.dribbble.com/users/6047818/screenshots/17678900/media/c102bc8e88ea57a5945c2b7b96eb952a.png?resize=768x576&vertical=center',
+               url:'https://www.gaama.top/',
                platform:'Netlify',
+               platformurl:''
           },
           {
-               name:'gaama.top',
-               Owned:'gaama.top',
-               image:'https://img.freepik.com/free-photo/view-3d-retro-computer-with-cathode-ray-tube_23-2150713957.jpg?t=st=1719332971~exp=1719336571~hmac=dce445fb1d2df1d90863c00d40e79add43d19b86d284f26fb6b92a02f1adeed6&w=740',
-               url:'',
+               name:'Gaama.top',
+               owned:'gaama.top',
+               ownedurl:'',
+               image:'https://cdn.dribbble.com/userupload/14535006/file/original-8ab1cc408feffbf310a53e9f8396b57a.jpg?resize=1022x767&vertical=center',
+               url:'https://www.gaama.top/',
                platform:'Netlify',
+               platformurl:''
           },
           {
-               name:'gaama.top',
-               Owned:'gaama.top',
-               image:'https://img.freepik.com/free-photo/retro-computer-desk-arrangement_23-2150244353.jpg?t=st=1719332996~exp=1719336596~hmac=c3c983861ac8ea0e62a08b100d4d9d213e8440f040f08444dc0ca3fb835fb5da&w=826',
-               url:'',
+               name:'Gaama.top',
+               owned:'gaama.top',
+               ownedurl:'',
+               image:'https://cdn.dribbble.com/userupload/5381347/file/original-4bb794f7a722d00caa85f6282ca88e62.png?resize=1200x900',
+               url:'https://www.gaama.top/',
                platform:'Netlify',
+               platformurl:''
+          },
+          {
+               name:'Gaama.top',
+               owned:'gaama.top',
+               ownedurl:'',
+               image:'https://cdn.dribbble.com/userupload/12396630/file/original-134e12dfd9c3c1702baa4ed2848e26b9.gif',
+               url:'https://www.gaama.top/',
+               platform:'Netlify',
+               platformurl:''
+          },
+          {
+               name:'Gaama.top',
+               owned:'gaama.top',
+               ownedurl:'',
+               image:'https://cdn.dribbble.com/users/8953545/screenshots/18563309/media/56643ab68608be6d418c535bbb833f55.png?resize=768x576&vertical=center',
+               url:'https://www.gaama.top/',
+               platform:'Netlify',
+               platformurl:''
+          },
+          {
+               name:'Gaama.top',
+               owned:'gaama.top',
+               ownedurl:'',
+               image:'https://cdn.dribbble.com/userupload/13820606/file/original-4f2f01468766d4e0962d2f0a08120af7.png?resize=1022x767&vertical=center',
+               url:'https://www.gaama.top/',
+               platform:'Netlify',
+               platformurl:''
+          },
+          {
+               name:'Gaama.top',
+               owned:'gaama.top',
+               ownedurl:'',
+               image:'https://cdn.dribbble.com/userupload/13820604/file/original-1d8294745f78595e58ed565ba7746e12.png?resize=1022x559&vertical=center',
+               url:'https://www.gaama.top/',
+               platform:'Netlify',
+               platformurl:''
+          },
+          {
+               name:'Gaama.top',
+               owned:'gaama.top',
+               ownedurl:'',
+               image:'https://cdn.dribbble.com/userupload/3276275/file/original-c4dabc2e801a04fbddb408f5bf048c0f.png?resize=1200x900',
+               url:'https://www.gaama.top/',
+               platform:'Netlify',
+               platformurl:''
           },
      ]
   return (
@@ -38,7 +91,7 @@ function Site() {
           <div className='flex flex-col items-start justify-center'>
                <input placeholder='Sites' type="search" name="search" id="search-site" className='rounded-lg h-8 outline-blue-400 bg-zinc-800 text-zinc-100 px-3 text-sm font-normal border border-clr2' />
                <ul className='flex sm:flex-row flex-col items-start sm:items-center justify-center sm:gap-5 mt-3'>
-                    <li className='font-light'>Owned by <a href="https://www.gaama.top/" target='_blank' className='font-medium'>Gaama.top</a></li>
+                    <li className='font-light'>owned by <a href="https://www.gaama.top/" target='_blank' className='font-medium'>Gaama.top</a></li>
                     <li className='font-light'>Created by <a href="https://www.github.com/Abhay7111/" target='_blank' className='font-medium'>Abhay7111</a></li>
                     <li className='font-light'>Access <a href="https://www.github.com/Abhay7111/" target='_blank' className='font-medium'>Any</a></li>
                     <a className='py-2 px-4 mt-3 sm:m-0 rounded-lg bg-zinc-800 hover:bg-zinc-700 transition-all border border-clr1 cursor-pointer'>Edit filters</a>
@@ -55,8 +108,8 @@ function Site() {
                </ul>
           </div>
      </div>
-     <div className='w-full h-auto bg-red-400/20 mt-5 '>
-     <div className='flex flex-wrap justify-start gap-1 gap-y-2'>
+     <div className='w-full h-auto mt-5 '>
+     <div className='flex flex-wrap justify-start gap-3 gap-y-5'>
           {sites.map((items, index)=>(
                <Sitecard sites={items}/>
           ))}
