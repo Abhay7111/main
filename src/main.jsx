aee0bd171385bb0ec0962cb61e722e2c0cfab826
Abhay vishwakarma <142080096+Abhay7111@users.noreply.github.com>
2024-10-11 20:37:56 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
@@ -79,7 +78,7 @@ const routes = createBrowserRouter([
   { path: '*', element: <Notfound /> },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={routes} />
   </React.StrictMode>
