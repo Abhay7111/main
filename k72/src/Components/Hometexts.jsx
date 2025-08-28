4fee36767e134dd2ef58f4c159ff233f50b17422
Abhay vishwakarma <142080096+Abhay7111@users.noreply.github.com>
2025-08-28 09:36:10 +0530

    updated home texts

--- k72/src/Components/Hometexts.jsx
+++ k72/src/Components/Hometexts.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import Bgvideo from './Bgvideo'
+import React from 'react';
+import Bgvideo from './Bgvideo';
 
 function Hometexts() {
   return (
     <div className=''>
-     <h1 className='w-full text-center flex flex-col items-center uppercase text-[8rem] font-medium leading-none'>L'étincelle <br /> <span className='flex items-center gap-3'>qui <div className='w-60 h-30 rounded-full overflow-hidden'><Bgvideo/></div>génère</span>  la créativité</h1>
+     <h1 className='w-full text-center flex flex-col items-center uppercase text-[10vw] leading-none font-[font1]'>
+          L'étincelle 
+          <br /> 
+          <span className='flex items-center gap-3'>qui <div className='w-[25vw] h-[10vw] rounded-full overflow-hidden'><Bgvideo/></div>génère</span>
+            <div className='relative'>la créativité <div className='absolute top-0 right-0 w-[70%] h-full -z-0 rounded-[50%] border-r border-b border-green-400 '></div></div>
+     </h1>
+     <div className='w-full flex items-center justify-end'>
+          <p className='w-68 font-[font1] text-sm md:text-lg mt-10 md:mt-5'> <span className='px-5'></span> K72 est une agence qui pense chaque action pour nourrir la marque. Demain, dans 5 mois et dans 5 ans. On cherche la friction qui crée l’étincelle pour générer de l’émotion. Pour assurer une relation honnête, on est sans filtre, on dit ce qui doit être dit, on fait ce qui doit être fait.</p>
+     </div>
     </div>
   )
 }
--- k72/src/Pages/Home.jsx
+++ k72/src/Pages/Home.jsx
@@ -9,11 +9,11 @@ function Home() {
     <div className='w-full h-screen overflow-auto'>
      <div className='w-full h-screen fixed bg-transparent -z-0'><Bgvideo/></div>
      <div className='w-full h-full z-10 relative'>
-          <div className='flex items-start justify-between'>
+          <div className='flex items-start justify-between fixed top-0 left-0 w-full'>
                <NavLink to={`/`} className={`text-4xl font-bold px-4`}>k72</NavLink>
                <Nav/>
           </div>
-          <div><Hometexts/></div>
+          <div className='md:pt-14 pt-25 '><Hometexts/></div>
      </div>
     </div>
   )
